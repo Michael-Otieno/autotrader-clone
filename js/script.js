8365dc3b915bd4259d6192504a91ee17c067b7d6
Michael-Otieno <m.otieno205@gmail.com>
2022-03-30 12:35:21 +0300

    add accordions and styles

--- js/script.js
+++ js/script.js
@@ -39,30 +39,51 @@ $('.slider-nav').slick({
     }
   })
 
+$(document).ready(function(){
+  $(".accordion- p").click(function(){
+    var id = this.id
+    $(".accordion-content").each(function(){
+      if($("#"+id).next()[0].id!=this.id){
+        $(this).slideUp();
+      }
+    });
+    $(this).next().toggle();
+  })
+})
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myModalBtn");
-// var container = document.querySelector('.container-fluid-body')
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  // container.style.overflow="hidden";
 
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+
+
+
+// // Get the modal
+// var modal = document.getElementById("myModal");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myModalBtn");
+// // var container = document.querySelector('.container-fluid-body')
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+//   // container.style.overflow="hidden";
+
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
